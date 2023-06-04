4a8e58aec3b29b40872f6c0c623adff3eaade1f9
rgruevski <robgruevski@gmail.com>
2023-06-04 16:27:23 -0400

    If else statements with atoi function from stdlib.h

--- app.c
+++ app.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-int main() {
+int main(int argc, char *argv[]) {
 
-    int a;
-    printf("Type an integer: ");
-    scanf("%d", &a);
-    if (a > 10) printf("%d is greater than 10.\n",a);
+    int a = atoi(argv[1]); // atoi converts a string to an integer
+    // printf("Type an integer: ");
+    // scanf("%d", &a);
+    if (a > 10) {
+        printf("%d is greater than 10.\n",a);
+    }
+    else if(a <= 10) {
+        printf("%d is less than or equal to 10.\n",a);
+    }
+    else 
+    {
+        printf("This is the else statement.\n");
+    }
 
     return (0);
 }

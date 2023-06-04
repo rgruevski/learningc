c51a13356952971a950731a83e63492bb6f4f3e7
rgruevski <robgruevski@gmail.com>
2023-06-04 14:59:36 -0400

    Taking in arguments

--- app.c
+++ app.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
+// argc is the number of arguments passed into the program
+// argv is an array of pointers to the arguments
+int main(int argc, char *argv[]) {
 
-int main() {
+  for (int i = 1; i < argc; i++) {
+    printf("Argument %d: %s\n", i, argv[i]);
+  }
 
-    int x;
-    int y = 5;
-    int z;
-    printf("Input an integer to be subtracted from 5:");
-    scanf("%d", &x);
-    z = x - y;
-    printf("%c\n",z);
-    printf("%d - %d = %d\n",x,y,z);
-
-    return (0);
+  return 0;
 }

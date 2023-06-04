f6b6430660da046ddde3f050ad45c5b427b4d89d
rgruevski <robgruevski@gmail.com>
2023-06-04 14:36:37 -0400

    Doing math

--- app.c
+++ app.c
@@ -2,12 +2,14 @@
 
 int main() {
 
-    char input[64]; // 15 is the maximum number of characters that can be stored in the array
-
-    printf("Instructions: ");
-    fgets(input, 64, stdin); // 64 is the maximum number of characters that can be stored in the array, stdin is the standard input.
-    puts("Thank you, here are your instructions:");
-    puts(input); // prints the string
+    int x;
+    int y = 5;
+    int z;
+    printf("Input an integer to be subtracted from 5:");
+    scanf("%d", &x);
+    z = x - y;
+    printf("%c\n",z);
+    printf("%d - %d = %d\n",x,y,z);
 
     return (0);
 }

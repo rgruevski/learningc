c69f12717ebf7cc5338300d4d99f2b9b1edf1b01
rgruevski <robgruevski@gmail.com>
2023-06-19 19:53:15 -0400

    outputting a char array

--- app.c
+++ app.c
@@ -2,19 +2,15 @@
 #define MEALS 3
 int main(int argc, char *argv[])
 {
-    int calories[MEALS];
-    int x,total;
+    char hello[] = "Greetings, earthling!\n";
+    int n;
 
-    total = 0;
-    puts("Calorie Counter");
-
-    for (x = 0; x < MEALS; x++)
+    n = 0;
+    while(hello[n] != '\0')
     {
-        printf("Calories at meal %d: ", x + 1);
-        scanf("%d", &calories[x]);
-        total += calories[x];
+        putchar(hello[n]);
+        n++;
     }
-    printf("You had a total of %d calories.\n", total);
 
     return (0);
 }

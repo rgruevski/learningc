94d8e076d218d1dd31c7d3b308b1b9d644d4dc2c
rgruevski <robgruevski@gmail.com>
2023-06-20 13:33:13 -0400

    Understanding structures

--- app.c
+++ app.c
@@ -5,14 +5,22 @@ int main()
         int account;
         float balance;
     };
-    struct bank checking;
+    struct bank checking,savings;
 
     checking.account = 1234;
     checking.balance = 567.89;
+    savings.account = 9876;
+    savings.balance = 543.21;
 
     printf("Checking account %d has a balance of %f\n",
         checking.account, 
-        checking.balance);
+        checking.balance
+    );
+
+    printf("Savings account %d has a balance of %f\n",
+        savings.account, 
+        savings.balance
+    );    
 
     return (0);
 }

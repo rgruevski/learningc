21d20a4708822b5f9de5d796084116bf1a6b678a
rgruevski <robgruevski@gmail.com>
2023-06-19 20:08:04 -0400

    Creating multidimensional arrays

--- app.c
+++ app.c
@@ -2,15 +2,19 @@
 #define MEALS 3
 int main(int argc, char *argv[])
 {
-    char hello[] = "Greetings, earthling!\n";
-    int n;
+    int grid[3][3];
+    int row,col;
 
-    n = 0;
-    while(hello[n] != '\0')
-    {
-        putchar(hello[n]);
-        n++;
-    }
+    for(row=0;row<3;row++)
+        for(col=0;col<3;col++)
+            grid[row][col] = row*col;
+
+    grid[2][2] = 99;
+
+    for(row=0;row<3;row++)
+        for(col=0;col<3;col++)
+            printf("grid[%d][%d] = %d\n",row,col,grid[row][col]);
+            putchar('\n');
 
     return (0);
 }
